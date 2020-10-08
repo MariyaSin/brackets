002603772ad1b5b0c73eac4d1888eb1342eff574
MariyaSin <maria1991@list.ru>
2020-10-08 05:13:25 +0300

    feat: fixed check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  str = str.split('');
+
+  for (let symb = 0; symb < str.length; symb++){
+    
+    point: for (let i = 0; i < bracketsConfig.length; i++){
+      if (bracketsConfig[i][0] === str[symb] && stack[stack.length - 1] !== bracketsConfig[i][1]){
+        stack.push(str[symb]);
+        break point;
+      }
+      if (bracketsConfig[i][1] === str[symb] && stack[stack.length - 1] === bracketsConfig[i][0]){
+        stack.pop();
+        break point;
+      }
+      if (bracketsConfig[i][1] === str[symb]){
+        return false;
+      }
+    }
+  }
+    return stack.length == 0;
 }
